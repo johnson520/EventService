bf2caa940ce4b882ac70717e99174651a3991e2e
Ted Johnson <johnson520@gmail.com>
2018-08-20 14:38:15 -0700

    Add canned api/calendars?email= endpoint

--- EventService/Controllers/CalendarsController.cs
+++ EventService/Controllers/CalendarsController.cs
@@ -13,8 +13,11 @@ namespace EventService.Controllers
         [Route("api/calendars")]
         public List<StormHacks.CalendarsOwned> GetCalendars([FromUri] string email)
         {
-            return StormHacks.GetOwnedCalendars(email);
+         //   return StormHacks.GetOwnedCalendars(email);
+            return JsonConvert.DeserializeObject<List<StormHacks.CalendarsOwned>>(cals).Where(c => c.CalendarDisplayName != "Default Field Owner").ToList();
         }
+
+        private const string cals = "[{\"CalendarDisplayName\":\"Apollo\'s Calendar\",\"CalendarID\":51,\"ShowPredefined\":false},{\"CalendarDisplayName\":\"Linda\'s Calendar\",\"CalendarID\":72,\"ShowPredefined\":true},{\"CalendarDisplayName\":\"Ted\'s Calendar\",\"CalendarID\":73,\"ShowPredefined\":false},{\"CalendarDisplayName\":\"Default Field Owner\",\"CalendarID\":74,\"ShowPredefined\":false},{\"CalendarDisplayName\":\"Matt\'s Calendar\",\"CalendarID\":75,\"ShowPredefined\":true},{\"CalendarDisplayName\":\"Ryan\'s Calendar\",\"CalendarID\":76,\"ShowPredefined\":true}]";
 
         [HttpGet]
         [Route("api/eventtypes/{calId}")]
